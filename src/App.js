a2823d94aa7145ba6fd221a914e16601210abc6b
SUDIP MODI <sudipmodi5297@gmail.com>
2021-04-02 19:52:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import data from "./data";
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    e.preventDefault();
+    setText(data.slice(0, amount));
+  };
+
+  return (
+    <section className="section-center">
+      <h2>Tired of boring Lorem Ipsum?</h2>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          type="number"
+          id="amount"
+          name="amount"
+          value={count}
+          onChange={(e) => {
+            setCount(e.target.value);
+          }}
+        ></input>
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
+}
+
+export default App;
